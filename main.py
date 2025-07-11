4f2fd0876f95a66d89ed6cc5a88dd9cf81717852
the-groovester <christian.grove.knutsen@gmail.com>
2025-07-11 08:32:52 +0200

    003: get_book_text function

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("greetings boots")
+def get_book_text(filepath):
+    with open(filepath) as f:
+        book_content = f.read()
+        return book_content
+    
+def main(path):
+    print(get_book_text(path))
+
+main("books/frankenstein.txt")
