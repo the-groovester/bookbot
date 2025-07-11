34c5686ff2ffb06acc05c3a3411c9b1acd5d0520
the-groovester <christian.grove.knutsen@gmail.com>
2025-07-11 08:59:02 +0200

    004: get_word_count

--- main.py
+++ main.py
@@ -2,8 +2,14 @@ def get_book_text(filepath):
     with open(filepath) as f:
         book_content = f.read()
         return book_content
-    
+
+def get_word_count(book_string):
+    return len(book_string.split())
+
+
 def main(path):
-    print(get_book_text(path))
+    num_words = get_word_count(get_book_text(path))
+
+    print(f"{num_words} words found in the document")
 
 main("books/frankenstein.txt")
