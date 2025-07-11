08a4600b9810cb804e31a7ac6ff433aca4cc0335
the-groovester <christian.grove.knutsen@gmail.com>
2025-07-11 09:34:57 +0200

    006: get_characters

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import get_word_count
+from stats import get_characters
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -10,5 +11,6 @@ def main(path):
     num_words = get_word_count(get_book_text(path))
 
     print(f"{num_words} words found in the document")
+    print(get_characters(get_book_text(path)))
 
 main("books/frankenstein.txt")
--- stats.py
+++ stats.py
@@ -1,2 +1,16 @@
 def get_word_count(book_string):
     return len(book_string.split())
+
+def get_characters(book_string):
+    #function takes a string and lowers it
+    #the function then initiates a dictionary and iterates over the lowered string
+    #if the character is not in the list of key-values, it updates the dictionary with the key
+    #if the key is in the dictionary, it adds 1 to the value
+    char_dict = {}
+    for char in book_string.lower():
+        if char not in char_dict.keys():
+            char_dict.update({char:1})
+        elif char in char_dict.keys():
+            char_dict[char] += 1
+
+    return char_dict
